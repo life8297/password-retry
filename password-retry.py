5c6cc1eddedafdc3f7a90248ea83e9872cc54d4c
JR <allen8297@gmail.com>
2020-07-09 23:09:42 +0800

    update wrong times = 0

--- password-retry.py
+++ password-retry.py
@@ -1,14 +1,14 @@
 # 密碼重試程式
 x = 3
 password = 'a123'
-while True:
+while x > 0:
 	pwd = input('請輸入密碼:')
 	if pwd == password:
-		print('登入成功')
+		print('登入成功!')
 		break
 	else:
 		x = x - 1
-		print('密碼錯誤請重新輸入, 還有', x, '次機會')	
-		if x == 0:
-			print('帳戶已凍結')
-			break
+		if x > 0:
+			print('密碼錯誤請重新輸入, 還有', x, '次機會')	
+		else:
+			print('密碼錯誤3次, 帳戶已凍結')
